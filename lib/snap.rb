2b58fba2d2f252693d5fa010ba9bb93900d8c979
Andrew Hood <andrewhood125@gmail.com>
2018-09-15 23:50:30 -0700

    Add Snap shipment model
    This also pulls in Hashie to give us some free form hashes with required
    properties.

--- lib/snap.rb
+++ lib/snap.rb
@@ -1,12 +1,16 @@
+require 'hashie'
 require 'httparty'
 
 require 'snap/version'
 require 'snap/client'
 
 require 'snap/api/shipments'
+require 'snap/models/shipment'
 
 # Top level module for the Snap gem.
 module Snap
+  include Models
+
   class << self
     attr_accessor :config
   end
--- snap.gemspec
+++ snap.gemspec
@@ -18,6 +18,7 @@ Gem::Specification.new do |spec|
   end
   spec.require_paths = ['lib']
 
+  spec.add_dependency 'hashie', '~> 3.5'
   spec.add_dependency 'httparty', '~> 0.16'
 
   spec.add_development_dependency 'bundler', '~> 1.16'

24300509bdd03996b9ff68557b881e719ac6657e
Andrew Hood <andrewhood125@gmail.com>
2018-09-16 13:44:40 -0700

    --wip-- [skip ci]

--- lib/snap/client.rb
+++ lib/snap/client.rb
@@ -6,7 +6,24 @@ module Snap
     def client
       base_uri Snap.config.endpoint
       basic_auth(Snap.config.username, Snap.config.password)
+      raise_on 500..599
       self
+    end
+
+    def get(*args, &block)
+      parse_response do
+        super(*args, &block)
+      end
+    end
+
+    def post(*args, &block)
+      parse_response do
+        super(*args, &block)
+      end
+    end
+
+    def parse_response
+      response = yield
     end
   end
 end
